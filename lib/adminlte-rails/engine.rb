73465182ebb86ee3233cc48968a0738c40215488
Nguyen Ba Dung <dung.nguyen@indexmedia.com>
2014-09-17 14:34:23 +0700

    fix assets precompile bug

--- lib/adminlte-rails/engine.rb
+++ lib/adminlte-rails/engine.rb
@@ -3,6 +3,8 @@ module AdminLTE
     class Engine < ::Rails::Engine
       initializer :images do |app|
         app.config.assets.precompile += %w( auth.css auth.js iCheck/flat/_all.css )
+        app.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+        app.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
       end
     end
   end
